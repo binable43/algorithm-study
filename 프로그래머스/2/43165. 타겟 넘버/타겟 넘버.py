fbeb3d77f0052a2157d44e1b5420456eb6639ba6
EunBin Lee <81629116+binable43@users.noreply.github.com>
2025-04-08 22:49:25 +0900

    [level 2] Title: 타겟 넘버, Time: 470.16 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,19 @@
+def solution(array, target):
+    all_ways = []
+
+    def get_all_ways(array, current_index, current_sum):
+        if current_index == len(array):
+            all_ways.append(current_sum)
+            return
+        get_all_ways(array, current_index + 1, current_sum + array[current_index])
+        get_all_ways(array, current_index + 1, current_sum - array[current_index])
+
+    get_all_ways(array, 0, 0)
+
+    target_count = 0
+
+    for way in all_ways:
+        if target == way:
+            target_count += 1
+
+    return target_count
